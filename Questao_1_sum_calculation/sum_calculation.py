cde68de7c0cd2cdb7069bef659ecae534d0692bf
GeorgeAlexsander <111101371+GeorgeAlexsander@users.noreply.github.com>
2024-09-25 01:08:22 -0300

    Add files via upload

--- Questao_1_sum_calculation/sum_calculation.py
+++ Questao_1_sum_calculation/sum_calculation.py
@@ -2,15 +2,12 @@
 
 ## Descrição da Atividade
 
-Este programa calcula a soma dos números inteiros de 1 até um índice definido (neste caso, 13). A lógica é implementada usando um loop `while`, que itera até que um contador (`K`) atinja o valor do índice. Durante cada iteração, o valor de `K` é adicionado à variável `SOMA`.
+#Este programa calcula a soma dos números inteiros de 1 até um índice definido (neste caso, 13). A lógica é implementada usando um loop `while`, que itera até que um contador (`K`) atinja o valor do índice. Durante cada iteração, o valor de `K` é adicionado à variável `SOMA`.
 
 ## Objetivo
 
-O objetivo desta atividade é demonstrar o uso de estruturas de repetição e variáveis em Python para realizar cálculos simples. Ao final da execução, o programa imprime o resultado da soma.
+#O objetivo desta atividade é demonstrar o uso de estruturas de repetição e variáveis em Python para realizar cálculos simples. Ao final da execução, o programa imprime o resultado da soma.
 
-## Código
-
-```python
 # -*- coding: utf-8 -*-
 """
 Created on Wed Sep 25 00:26:17 2024
